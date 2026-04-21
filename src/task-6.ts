127ad496402ea5776e0802571891880f42cdd19a
Evelina <evelina20082707@gmail.com>
2026-04-21 22:26:27 +0300

    add eight tasks

---
+++ src/task-6.ts
@@ -0,0 +1,9 @@
+function getFirstElement<T>(arr:T[]):T {
+  return arr[0];
+}
+
+getFirstElement([1, 2, 3]);           
+getFirstElement(["a", "b", "c"]);  
+getFirstElement([true, false, true]); 
+
+
